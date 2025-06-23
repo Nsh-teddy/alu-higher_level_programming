df7bd85b237ddf412253476d8c175739bac87155
Nsh-teddy <t.nshimyumu@alustudent.com>
2025-06-23 13:58:59 -0700

    Add file 8-uppercase.py to git repo

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
-        # Convert lowercase letter to uppercase by subtracting 32 from ASCII code
         if 'a' <= c <= 'z':
             print("{}".format(chr(ord(c) - 32)), end="")
         else:
