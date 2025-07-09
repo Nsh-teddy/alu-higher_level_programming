9d8c12dfc779f160a08009751767f8da752199ae
Nsh-teddy <t.nshimyumu@alustudent.com>
2025-07-09 06:45:05 -0700

    Add file 2-main.py to git repo

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -6,18 +6,18 @@ class Rectangle:
     """Represents a rectangle."""
 
     def __init__(self, width=0, height=0):
-        """Initialize the rectangle with optional width and height."""
+        """Initializes the rectangle with optional width and height."""
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """Retrieve the width."""
+        """Gets the width."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Set the width with validation."""
+        """Sets the width with validation."""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -26,12 +26,12 @@ class Rectangle:
 
     @property
     def height(self):
-        """Retrieve the height."""
+        """Gets the height."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Set the height with validation."""
+        """Sets the height with validation."""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -39,11 +39,21 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        """Return the area of the rectangle."""
+        """Returns the area of the rectangle."""
         return self.__width * self.__height
 
     def perimeter(self):
-        """Return the perimeter of the rectangle."""
+        """Returns the perimeter of the rectangle."""
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Returns the rectangle with the character '#'."""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        rect_lines = []
+        for _ in range(self.__height):
+            rect_lines.append("#" * self.__width)
+        return "\n".join(rect_lines)
+
