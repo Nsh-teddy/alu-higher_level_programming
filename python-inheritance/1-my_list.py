96729ec8eac976bc8fd5816b8963ac0966d893f7
Nsh-teddy <t.nshimyumu@alustudent.com>
2025-07-11 01:58:26 -0700

    Add 1-main.py

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Defines a class MyList that inherits from list.
+"""
+
+
+class MyList(list):
+    """
+    A subclass of list with a public method to print the sorted list.
+    """
+
+    def print_sorted(self):
+        """
+        Prints the list in ascending sorted order without modifying the original.
+        """
+        print(sorted(self))
