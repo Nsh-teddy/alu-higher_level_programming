b87acc99c0871123e705f7878a224d78885b3e1a
Nsh-teddy <t.nshimyumu@alustudent.com>
2025-07-14 05:48:55 -0700

    Add file

---
+++ python-input_output/9-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Module that defines a Student class with JSON serialization method.
+"""
+
+
+class Student:
+    """
+    Defines a student by first name, last name, and age.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a new Student instance.
+
+        Args:
+            first_name (str): First name of the student.
+            last_name (str): Last name of the student.
+            age (int): Age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of the Student instance.
+
+        Returns:
+            dict: Dictionary of serializable attributes.
+        """
+        return self.__dict__
